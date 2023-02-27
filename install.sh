a4aa1c65da1d521d9466fe0380b36583db6d0dba
kriskoribsky <kiko.koribsky@gmail.com>
2023-02-27 01:05:01 +0100

    add git symlink

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@
 ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/bash/.git-prompt.sh ~/.git-prompt.sh
 
+# git
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
 # imwheel
 ln -s ~/.dotfiles/popos/.imwheelrc ~/.imwheelrc
 
